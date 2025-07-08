c73b4d06c2cac07dfe8012aa8c9acaaff6dfe2ac
Manjur <manjursk209@gmail.com>
2025-07-08 17:41:44 +0530

    dev - project setup

---
+++ images.d.ts
@@ -0,0 +1,26 @@
+// images.d.ts
+declare module "*.png" {
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module "*.jpg" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.jpeg" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.gif" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.svg" {
+    const value: any;
+    export default value;
+}
+
---
+++ type.d.ts
@@ -0,0 +1,5 @@
+export interface TabBarIconProps {
+    focused: boolean;
+    icon: ImageSourcePropType;
+    title: string;
+}
