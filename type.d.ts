7c19a69a99e2b22973f50f8994708a182ce4cf95
Manjur <manjursk209@gmail.com>
2025-07-10 23:44:08 +0530

    Cart Buttom component

--- type.d.ts
+++ type.d.ts
@@ -26,3 +26,43 @@ interface User extends Models.Document {
     email: string;
     avatar: string;
 }
+
+interface CartCustomization {
+    id: string;
+    name: string;
+    price: number;
+    type: string;
+}
+interface CartStore {
+    items: CartItemType[];
+    addItem: (item: Omit<CartItemType, "quantity">) => void;
+    removeItem: (id: string, customizations: CartCustomization[]) => void;
+    increaseQty: (id: string, customizations: CartCustomization[]) => void;
+    decreaseQty: (id: string, customizations: CartCustomization[]) => void;
+    clearCart: () => void;
+    getTotalItems: () => number;
+    getTotalPrice: () => number;
+}
+
+interface Category extends Models.Document {
+    $id: string;
+    name: string;
+    description: string;
+}
+
+export interface MenuItem extends Models.Document {
+    $id: string;
+    name: string;
+    price: number;
+    image_url: string;
+    description: string;
+    calories: number;
+    protein: number;
+    rating: number;
+    type: string;
+}
+
+interface GetMenuParams {
+    category: string;
+    query: string;
+}
