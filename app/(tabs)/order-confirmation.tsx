a9561ada14422ac1760c4b08bb9b483cfa014614
Manjur <manjursk209@gmail.com>
2025-07-27 18:44:10 +0530

    dev - updated order confirmation component

---
+++ app/(tabs)/order-confirmation.tsx
@@ -0,0 +1,10 @@
+import OrderSuccess from '@/components/OrderSuccess'
+import React from 'react'
+
+const OrderConfirmation = () => {
+  return (
+    <OrderSuccess />
+  )
+}
+
+export default OrderConfirmation
