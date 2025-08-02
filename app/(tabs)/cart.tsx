704cd49e2ea708812e3a1c7038352e4abbe81117
Manjur <manjursk209@gmail.com>
2025-08-02 16:49:57 +0530

    cart module updated

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,31 +1,24 @@
+import BillingInformation from "@/components/BillingInformation";
 import CartItem from "@/components/CartItem";
-import CheckoutScreen from "@/components/CheckoutScreen";
+import CustomButton from "@/components/CustomButton";
 import CustomHeader from "@/components/CustomHeader";
+import PaymentSummary from "@/components/PaymentSummary";
 import { useCartStore } from "@/store/cart.store";
-import { PaymentInfoStripeProps } from "@/type";
-import cn from "clsx";
-import { FlatList, Text, View } from 'react-native';
+import { useState } from "react";
+import { FlatList, KeyboardAvoidingView, Platform, ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const PaymentInfoStripe = ({ label,  value,  labelStyle,  valueStyle, }: PaymentInfoStripeProps) => (
-    <View className="flex-between flex-row my-1">
-        <Text className={cn("paragraph-medium text-gray-200", labelStyle)}>
-            {label}
-        </Text>
-        <Text className={cn("paragraph-bold text-dark-100", valueStyle)}>
-            {value}
-        </Text>
-    </View>
-);
-
 const Cart = () => {
-    const { items, getTotalItems, getTotalPrice } = useCartStore();
+    const { items, getTotalItems } = useCartStore();
+    
+    const [cartView, setCartView] = useState<'payment-summary' | 'billing-information'>('payment-summary')
 
     const totalItems = getTotalItems();
-    const totalPrice = getTotalPrice();
 
     return (
         <SafeAreaView className="bg-white h-full">
+          {
+            cartView === 'payment-summary' ?
             <FlatList
                 data={items}
                 renderItem={({ item }) => <CartItem item={item} />}
@@ -35,38 +28,21 @@ const Cart = () => {
                 ListEmptyComponent={() => <Text>Cart Empty</Text>}
                 ListFooterComponent={() => totalItems > 0 && (
                     <View className="gap-5">
-                        <View className="mt-6 border border-gray-200 p-5 rounded-2xl">
-                            <Text className="h3-bold text-dark-100 mb-5">
-                                Payment Summary
-                            </Text>
+                        <PaymentSummary />
 
-                            <PaymentInfoStripe
-                                label={`Total Items (${totalItems})`}
-                                value={`$${totalPrice.toFixed(2)}`}
-                            />
-                            <PaymentInfoStripe
-                                label={`Delivery Fee`}
-                                value={`$5.00`}
-                            />
-                            <PaymentInfoStripe
-                                label={`Discount`}
-                                value={`- $0.50`}
-                                valueStyle="!text-success"
-                            />
-                            <View className="border-t border-gray-300 my-2" />
-                            <PaymentInfoStripe
-                                label={`Total`}
-                                value={`$${(totalPrice + 5 - 0.5).toFixed(2)}`}
-                                labelStyle="base-bold !text-dark-100"
-                                valueStyle="base-bold !text-dark-100 !text-right"
-                            />
-                        </View>
-
-                        {/* <CustomButton title="Order Now" /> */}
-                        <CheckoutScreen />
+                        <CustomButton title="Proceed" onPress={() => {setCartView('billing-information')}} />
                     </View>
                 )}
             />
+            : <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+                <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled"> 
+                  <View className=" pb-28 px-5 pt-5 gap-5">
+                    <PaymentSummary />
+                    <BillingInformation {...{setCartView}} />
+                  </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
+          }
         </SafeAreaView>
     )
 }
