852335bba9a7fa759d8611676b81b8f3452cfe4e
Manjur <manjursk209@gmail.com>
2025-09-06 17:57:43 +0530

    updated Custom Button Component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -9,17 +9,21 @@ const CustomButton = ({
     style,
     textStyle,
     leftIcon,
-    isLoading = false
+    isLoading = false,
+    variant = "primary",
 }: CustomButtonProps) => {
+    const isPrimary = variant === "primary"
+    const primaryStyle = "bg-primary text-white border-0";
+    const secondaryStyle = "bg-white border border-gray-300 text-black";
     return (
-        <TouchableOpacity className={cn('custom-btn', isLoading ? "opacity-50" : "", style)} disabled={isLoading} onPress={onPress}>
+        <TouchableOpacity className={cn('custom-btn', isPrimary ? primaryStyle : secondaryStyle, isLoading ? "opacity-50" : "", style)} disabled={isLoading} onPress={onPress}>
             {leftIcon}
 
             <View className="flex-center flex-row">
                 {isLoading ? (
-                    <ActivityIndicator size="small" color="white" />
+                    <ActivityIndicator size="small" color={isPrimary ? "white" : "black"} />
                 ): (
-                    <Text className={cn('text-white-100 paragraph-semibold', textStyle)}>
+                    <Text className={cn('paragraph-semibold', isPrimary ? "text-white-100" : "text-black-100", textStyle)}>
                         {title}
                     </Text>
                 )}
