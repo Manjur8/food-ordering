5f96b2e9987581dd4926a44d202e661b409c84e3
Manjur <manjursk209@gmail.com>
2025-09-07 22:28:09 +0530

    set queries for view food route

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,6 +1,7 @@
 import { appWriteConfig } from "@/lib/appWrite";
 import { useCartStore } from "@/store/cart.store";
 import { MenuItem } from "@/type";
+import { router } from "expo-router";
 import { Image, Platform, Text, TouchableOpacity } from 'react-native';
 
 const MenuCard = ({ item: { $id, image_url, name, price }}: { item: MenuItem}) => {
@@ -8,7 +9,10 @@ const MenuCard = ({ item: { $id, image_url, name, price }}: { item: MenuItem}) =
     const { addItem } = useCartStore();
 
     return (
-        <TouchableOpacity className="menu-card" style={Platform.OS === 'android' ? { elevation: 10, shadowColor: '#878787'}: {}}>
+        <TouchableOpacity className="menu-card" style={Platform.OS === 'android' ? { elevation: 10, shadowColor: '#878787'}: {}} onPress={() => router.push({
+            pathname: "/products/[id]",
+            params: {id: $id}
+        })}>
             <Image source={{ uri: imageUrl }} className="size-32 absolute -top-10" resizeMode="contain" />
             <Text className="text-center base-bold text-dark-100 mb-2" numberOfLines={1}>{name}</Text>
             <Text className="body-regular text-gray-200 mb-4">From ${price}</Text>
